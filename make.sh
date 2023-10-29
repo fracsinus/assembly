d87219647d6aa79b62dec76eb2df8accbbef729a
Ahn Shinho <fracsinus@gmail.com>
2023-10-30 00:48:20 +0900

    build: Add build script

---
+++ make.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+DIRNAME=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
+OUTPUT_DIR="$DIRNAME/out"
+
+FILENAME=$(basename $1)
+EXTENSION=${FILENAME#*.}
+if [[ "$EXTENSION" != "asm" ]]; then
+  echo "[ERROR] file extension must be .asm"
+  exit 1
+fi
+BASE_NAME=${FILENAME%.*}
+
+mkdir -p "$DIRNAME/out"
+
+nasm -f elf64 "$1" -o "/tmp/$BASE_NAME.o"
+ld "$OUTPUT_DIR/$BASE_NAME.o" -o "$BASE_NAME.out"
